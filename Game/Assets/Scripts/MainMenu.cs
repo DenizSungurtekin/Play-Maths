8f896d991ba5a7cab318681ab4727223fd8e1d4f
Sajay <sajaendra.thevamanoharan@etu.unige.ch>
2021-06-02 19:51:29 +0200

    NameAgeInput Added

--- Game/Assets/Scripts/MainMenu.cs
+++ Game/Assets/Scripts/MainMenu.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class MainMenu : MonoBehaviour
 {
+
+    public TMP_InputField name;
+    public TMP_InputField age;
+
+    public static string nameInput;
+    public static string ageInput;
+
+    public void Start() {
+
+        name = GameObject.Find("Name").GetComponent<TMP_InputField>();
+        age = GameObject.Find("Age").GetComponent<TMP_InputField>();
+
+
+    }
     public void PlayGame()
     {
+        // need a verification part here
+        nameInput = name.text;
+        ageInput = age.text;
         SceneManager.LoadScene(1);
     }
 }
--- Game/Assets/Scripts/ScoreCard.cs
+++ Game/Assets/Scripts/ScoreCard.cs
@@ -6,16 +6,28 @@ using TMPro;
 public class ScoreCard : MonoBehaviour
 {
     public TextMeshProUGUI textMesh;
+    public TextMeshProUGUI nameCard;
+    public TextMeshProUGUI ageCard;
+
     public int score ;
+    public string name;
+    public string age;
 
     void Start()
     {
-        textMesh = GetComponent<TextMeshProUGUI>();
+        textMesh =  GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+        nameCard = GameObject.Find("Name").GetComponent<TextMeshProUGUI>();
+        ageCard = GameObject.Find("Age").GetComponent<TextMeshProUGUI>();
+
+        name = MainMenu.nameInput;
+        age = MainMenu.ageInput;
     }
 
     void Update() {
         score = ScoreScript.scorefinal;
         textMesh.text = "Score : " + score.ToString();
+        nameCard.text = "Name : " + name;
+        ageCard.text = "Age : " + age;
     }
 
 }
